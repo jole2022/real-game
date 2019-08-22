dcc21e5880d11cffffb8dca3b93d9467855e1fd8
Joshua Lee <joshualee@1-168-0-218.dynamic-ip.hinet.net>
2019-08-22 18:04:32 +0900

    real game

--- counterfeit metalslug.py
+++ counterfeit metalslug.py
@@ -19,7 +19,7 @@ class llama:
 
     def move(self):
         canvas.move(self.image, -30, 0)
-
+#slime jump
 class slime:
     def __init__(self):
         self.image = canvas.create_image(1450, 550, image=mob1image)
@@ -48,26 +48,49 @@ class slime:
 class Boss:
 
     def __init__(self):
-        self.image=canvas.create_image(1500, 500, image=bossimage)
+        self.image=canvas.create_image(2800, 500, image=bossimage)
         self.maxhp = 200
         self.hp=200
         self.active = False
+        self.attack_cooldown = 10
 
     def move(self):
         player_pos = canvas.coords(player)
         boss_pos = canvas.coords(self.image)
         distance = abs(boss_pos[0] - player_pos[0])
 
-        if distance > 100:
-            if player_pos[0] < boss_pos[0]:
-                canvas.move(self.image, -20, 0)
+        self.attack_cooldown -= 1
+
+        if player_pos[0] < boss_pos[0]:
+            canvas.move(self.image, -20, 0)
+        else:
+            canvas.move(self.image, 20, 0)
+
+            #ranged attack
+
+        if self.attack_cooldown <= 0:
+            if distance > 250:
+                self.attack_cooldown = 35
+                if len(inactive_fire_list) > 0:
+                    fire = inactive_fire_list[0]
+                    if player_pos[0] < boss_pos[0]:
+                        fire.direction = 1
+                    else:
+                        fire.direction = 0
+                    active_fire_list.append(fire)
+                    del inactive_fire_list[0]
+                    canvas.itemconfig(fire.image, state='normal')
+                    canvas.coords(fire.image, boss_pos[0], boss_pos[1] - 25)
             else:
-                canvas.move(self.image, 20, 0)
+                #melee attack
+                pass
 
+class Fire:
+    def __init__(self):
+        self.image = canvas.create_image(0, 0, image=fireimage)
+        self.direction = 0  # 1 == left, 0 == right
 
-
-
-
+#player movement
 def move_left(event):
     global moved_distance
     global boss_battle
@@ -78,10 +101,10 @@ def move_left(event):
             canvas.move(bg2, 30, 0)
 
             bg_pos = canvas.coords(bg)
-            if bg_pos[0] > 2100:
-                canvas.move(bg, -2800, 0)
-            elif bg_pos[0] < -700:
-                canvas.move(bg, 2800, 0)
+            if bg_pos[0] > 2100: # if a background image entirely exceeds the right-side border
+                canvas.move(bg, -2800, 0)  # move it to left side (2 times canvas size)
+            elif bg_pos[0] < -700: # if it exceeds the left-side border
+                canvas.move(bg, 2800, 0) # move it to right side
 
             bg_pos = canvas.coords(bg2)
             if bg_pos[0] > 2100:
@@ -91,7 +114,8 @@ def move_left(event):
             for mob in active_mob_list:
                 canvas.move(mob.image, 30, 0)
             moved_distance -= 30
-        elif player_pos[0]>0: canvas.move(player, -30,0)
+        elif player_pos[0]>0: # if boss battle started & player is inside the screen
+            canvas.move(player, -30,0)
 
 
 def move_right(event):
@@ -116,10 +140,12 @@ def move_right(event):
                 canvas.move(bg2, -2800, 0)
             elif bg_pos[0] < -700:
                 canvas.move(bg2, 2800, 0)
+
             for mob in active_mob_list:
                 canvas.move(mob.image, -30, 0)
             moved_distance += 30
-            if moved_distance >= 1 * 1400:
+
+            if moved_distance >= 4 * 1400:
                 boss_battle = True
                 boss = Boss()
                 active_mob_list.append(boss)
@@ -128,7 +154,7 @@ def move_right(event):
 
         elif player_pos[0]<1400: canvas.move(player, 30, 0)
 
-
+# cooldown of shooting
 shoot_cooldown = True
 
 def cooldown_reset():
@@ -148,7 +174,7 @@ def shooting():
             del inactive_bullet_list[0]
             canvas.itemconfig(bullet, state='normal')
             player_pos = canvas.coords(player)
-            canvas.coords(bullet, (player_pos[0]) + 70, player_pos[1] -5)
+            canvas.coords(bullet, (player_pos[0]) + 60, player_pos[1] -5)
 
 
 def shootevent(event):
@@ -158,12 +184,13 @@ def shootevent(event):
         shoot_cooldown = False
         canvas.after(900, cooldown_reset)
 
+#player jumping
 jump = False
 jumppower = -30
 jumpspeed = jumppower
 gravity = 4
 def jumping():
-    global jump, jumpspeed, gravity
+    global jump, jumpspeed, gravity, jumppower
     jump = True
     pos = canvas.coords(player)
     if pos[1] > 550:
@@ -183,7 +210,7 @@ def jumpevent(event):
         if not jump:
             jumping()
 
-
+#variables
 active_mob_list = []  #empty list
 inactive_mob_list = list()
 boss = None
@@ -192,6 +219,8 @@ moved_distance = 0
 boss_battle = False
 active_bullet_list = [] #empty list
 inactive_bullet_list = list()
+active_fire_list = []
+inactive_fire_list = list()
 
 
 def mob_initiate():
@@ -231,19 +260,35 @@ def game_loop():
                 inactive_mob_list.append(mob)
                 active_mob_list.remove(mob)
 
-        # if boss is not None:
-            # boss.move()
+        player_pos = canvas.coords(player)
+        boss_pos = None
+        if boss is not None:
+            boss_pos = canvas.coords(boss.image)
 
      # bullet movement
         for bullet in active_bullet_list:
-            canvas.move(bullet, 100, 0)
+            if boss is not None and boss_pos[0] < player_pos[0]:
+                canvas.move(bullet, -100, 0)
+            else:
+                canvas.move(bullet, 100, 0)
             bullet_x = canvas.coords(bullet)[0]
 
-            if bullet_x > 1450:
+            if bullet_x > 1450 or bullet_x < -50:
                 canvas.itemconfig(bullet, state='hidden')
                 inactive_bullet_list.append(bullet)
                 active_bullet_list.remove(bullet)
 
+        for fire in active_fire_list:
+            if fire.direction == 0:
+                canvas.move(fire.image, 50, 0)
+            else:
+                canvas.move(fire.image, -50, 0)
+            fire_x = canvas.coords(fire.image)[0]
+
+            if fire_x > 1450 or fire_x < -50:
+                canvas.itemconfig(fire.image, state='hidden')
+                inactive_fire_list.append(fire)
+                active_fire_list.remove(fire)
 
     #player collision
         player_pos = canvas.coords(player)
@@ -271,10 +316,25 @@ def game_loop():
                     active_bullet_list.remove(bullet)
                     mob.hp -= 10
                     if mob.hp <= 0:
+                        if mob is boss:
+                            canvas.itemconfig(clear_text, state='normal')
+
                         canvas.itemconfig(mob.image, state='hidden')
                         inactive_mob_list.append(mob)
                         active_mob_list.remove(mob)
 
+
+        for fire in active_fire_list:
+            fire_pos = canvas.coords(fire.image)
+            diff_x = abs(fire_pos[0] - player_pos[0])
+            diff_y = abs(fire_pos[1] - (player_pos[1]))
+
+            if diff_x < 70 and diff_y < 80:  # if there is an intersection between fire and player
+                canvas.itemconfig(fire, state='hidden')
+                inactive_fire_list.append(fire)
+                active_fire_list.remove(fire)
+                canvas.itemconfig(gameover_text, state='normal')
+                game_over = True
 
     canvas.after(100, game_loop)
 
@@ -307,6 +367,8 @@ if __name__ == '__main__':
     player = canvas.create_image(200,550, image=playergunimage)
     gameover_text = canvas.create_text(700, 336, text='GAME OVER', font=('times new roman', 108), fill='black')
     canvas.itemconfig(gameover_text, state='hidden')
+    clear_text = canvas.create_text(700, 336, text='GAME COMPLETE', font=('times new roman', 108), fill='pink')
+    canvas.itemconfig(clear_text, state='hidden')
 
     #monster initialization
     for i in range(3):
@@ -317,6 +379,10 @@ if __name__ == '__main__':
         bullet = canvas.create_image(0, 0, image=bulletimage)
         canvas.itemconfig(bullet, state='hidden')
         inactive_bullet_list.append(bullet)
+
+        fire = Fire()
+        canvas.itemconfig(fire.image, state='hidden')
+        inactive_fire_list.append(fire)
 
     canvas.bind_all("<Left>", move_left)
     canvas.bind_all("<Right>", move_right)
